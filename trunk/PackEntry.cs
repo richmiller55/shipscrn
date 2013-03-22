2ac533eb9469d58eaeb0f097a6c67215f92fd5e9
richmiller55 <richmiller55@9d946aaf-4471-1531-b4d3-a8a9015ed9cc>
2013-03-22 23:24:27 +0000

    date update

--- trunk/PackEntry.cs
+++ trunk/PackEntry.cs
@@ -51,7 +51,7 @@ namespace ShipScrn
         }
         void initArBatchNames()
         {
-            string baseDate = "031913";
+            string baseDate = "032213";
             this.ARNoBg = "P" + baseDate + "1";
             this.ARBg = "P" + baseDate + "2";
             this.ARNoBg2 = "P" + baseDate + "3";
